c25d0d5461258c4058b8cf1858c86a6acfd757e6
Mauricio Farfan <maufar402@gmail.com>
2019-12-19 17:45:04 -0600

    Adding EFModel

---
+++ MrPiattoClient/MrPiattoDB/Categories.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MrPiattoClient.MrPiattoDB
+{
+    public partial class Categories
+    {
+        public int Idcategories { get; set; }
+        public int Idrestaurant { get; set; }
+        public string Category { get; set; }
+        public virtual Restaurant IdrestaurantNavigation { get; set; }
+    }
+}
---
+++ MrPiattoClient/MrPiattoDB/PaymentOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace MrPiattoClient.MrPiattoDB
+{
+    public partial class PaymentOptions
+    {
+        public int IdpaymentOptions { get; set; }
+        public int Idrestaurant { get; set; }
+        public string PaymentOption { get; set; }
+
+        public virtual Restaurant IdrestaurantNavigation { get; set; }
+    }
+}
