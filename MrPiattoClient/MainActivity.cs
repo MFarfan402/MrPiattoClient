81ffd07fce6c63cb8cf41ebf94f028fc67516327
Mauricio Farfan <maufar402@gmail.com>
2019-12-19 18:14:15 -0600

    CRUD Database

--- MrPiattoClient/MainActivity.cs
+++ MrPiattoClient/MainActivity.cs
@@ -3,6 +3,7 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using MrPiattoClient.MrPiattoDB;
 
 namespace MrPiattoClient
 {
@@ -11,17 +12,30 @@ namespace MrPiattoClient
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
-
+            
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public void consulta()
+        {
+            Restaurant restaurant = new Restaurant();
+            restaurant.Mail = "@mrpiatto.com";
+            restaurant.Password = "123";
+            restaurant.Confirmation = false;
+            using (var db = new MrPiattoContext())
+            {
+                db.Restaurant.Add(restaurant);
+            }
         }
     }
 }
