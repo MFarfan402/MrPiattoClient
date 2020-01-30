8a8026190b7dc16c579646c20002adb8e30ef328
Mauricio Farfan <maufar402@gmail.com>
2020-01-29 23:05:40 -0600

    Fixing Tooolbar issues. Added GoogleMaps API. Creating booking activity with calendar fragment

---
+++ MrPiattoClient/BookingActivity.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Support.V7.App;
+using Toolbar = Android.Support.V7.Widget.Toolbar;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace MrPiattoClient
+{
+    [Activity(Label = "BookingActivity")]
+    public class BookingActivity : AppCompatActivity
+    {
+        EditText editTextQuantity, editTextDate, editTextHour;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_bookATable);
+            InitToolbar();
+
+            editTextQuantity = FindViewById<EditText>(Resource.Id.editTextQuantity);
+            editTextDate = FindViewById<EditText>(Resource.Id.editTextDate);
+            editTextHour = FindViewById<EditText>(Resource.Id.editTextHour);
+
+            editTextDate.Click += delegate
+            {
+                DateTime today = DateTime.Today;
+                DatePickerDialog dialog = new DatePickerDialog(this, OnDateSet, today.Year, today.Month - 1, today.Day);
+                dialog.DatePicker.MinDate = today.Millisecond;
+                dialog.Show();
+            };
+        }
+
+        private void OnDateSet(object sender, DatePickerDialog.DateSetEventArgs e)
+        {
+            editTextDate.Text = e.Date.ToLongDateString();
+        }
+
+        private void InitToolbar()
+        {
+            Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.toolbarBooking);
+            SetSupportActionBar(toolbar);
+            SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+            SupportActionBar.Title = "Haz tu reservación";
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Android.Resource.Id.Home:
+                    Finish();
+                    return true;
+
+                default:
+                    return base.OnOptionsItemSelected(item);
+            }
+        }
+    }
+}
--- MrPiattoClient/MainActivity.cs
+++ MrPiattoClient/MainActivity.cs
@@ -1,4 +1,5 @@
 ﻿using Android.App;
+using Android.Support.V4.App;
 using Android.OS;
 using Android.Support.V7.App;
 using Android.Support.Design.Widget;
@@ -8,18 +9,90 @@ using Android.Views;
 using Android.Widget;
 using Toolbar = Android.Support.V7.Widget.Toolbar;
 using Android.Content;
+using Android.Gms.Maps;
 
 namespace MrPiattoClient
 {
-   [Activity(MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    [Activity(MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, IOnMapReadyCallback
     {
+        private MapView mapView;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
 
+            InitListeners();
+            InitMap(savedInstanceState);
+            InitToolbar();
+        }
+        protected override void OnResume()
+        {
+            base.OnResume();
+            mapView.OnResume();
+        }
+        protected override void OnStart()
+        {
+            base.OnStart();
+            mapView.OnStart();
+        }
+        protected override void OnStop()
+        {
+            base.OnStop();
+            mapView.OnStop();
+        }
+        protected override void OnPause()
+        {
+            mapView.OnPause();
+            base.OnPause();
+        }
+        protected override void OnDestroy()
+        {
+            mapView.OnDestroy();
+            base.OnDestroy();
+        }
+        public override void OnLowMemory()
+        {
+            base.OnLowMemory();
+            mapView.OnLowMemory();
+        }
+        public override void OnSaveInstanceState(Bundle outState, PersistableBundle outPersistentState)
+        {
+            base.OnSaveInstanceState(outState, outPersistentState);
+            Bundle mapViewBundle = outState.GetBundle("MapViewBundleKey");
+            if (mapViewBundle == null)
+            {
+                mapViewBundle = new Bundle();
+                outState.PutBundle("MapViewBundleKey", mapViewBundle);
+            }
+            mapView.OnSaveInstanceState(mapViewBundle);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_restaurant, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            Toast.MakeText(this, "Action selected: " + item.TitleFormatted,
+            ToastLength.Short).Show();
+            return base.OnOptionsItemSelected(item);
+        }
+        
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            googleMap.AddMarker(new Android.Gms.Maps.Model.MarkerOptions().SetPosition(new Android.Gms.Maps.Model.LatLng(0, 0)));
+        }
+
+        public void InitListeners()
+        {
             Button buttonSeeMorePhotos = FindViewById<Button>(Resource.Id.buttonSeeMorePhotos);
             buttonSeeMorePhotos.Click += delegate
             {
@@ -39,46 +112,61 @@ namespace MrPiattoClient
                 intent.SetData(Android.Net.Uri.Parse("tel:" + buttonCall.Text));
                 StartActivity(intent);
             };
-
-            Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
-            CollapsingToolbarLayout collapsingToolbar = FindViewById<CollapsingToolbarLayout>(Resource.Id.collapsingToolbar);
-            AppBarLayout appBarLayout = FindViewById<AppBarLayout>(Resource.Id.appBar);
-
-            collapsingToolbar.SetTitle("Esta es la prueba");
-            collapsingToolbar.SetExpandedTitleColor(GetColor(Resource.Color.mtrl_btn_transparent_bg_color));
-            collapsingToolbar.SetCollapsedTitleTextColor(Android.Resource.Color.Black);
-
-            /*
-            SetSupportActionBar(toolbar);
-            
-            toolbar.InflateMenu(Resource.Menu.menu_restaurant);
-            toolbar.MenuItemClick += (sender, e) =>
+            Button buttonBookATable = FindViewById<Button>(Resource.Id.btnBookATable);
+            buttonBookATable.Click += delegate
             {
-                Toast.MakeText(this, e.Item.TitleFormatted, ToastLength.Long).Show();
+                Intent intent = new Intent(this, typeof(BookingActivity));
+                StartActivity(intent);
             };
-            */
-
-        }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
         /*
-
-        public override bool OnCreateOptionsMenu(IMenu menu)
+        private void ButtonBookATable_Click(object sender, System.EventArgs e)
         {
-            MenuInflater.Inflate(Resource.Menu.menu_restaurant, menu);
-            return base.OnCreateOptionsMenu(menu);
-        }
 
-        public override bool OnOptionsItemSelected(IMenuItem item)
-        {
-            Toast.MakeText(this, "Action selected: " + item.TitleFormatted,
-                ToastLength.Short).Show();
-            return base.OnOptionsItemSelected(item);
+            Android.Support.V4.App.FragmentTransaction transaction = SupportFragmentManager.BeginTransaction();
+            BookingDialogFragment dialogFragment = new BookingDialogFragment();
+            dialogFragment.Show(transaction, "dialog fragment");
         }
         */
+        public void InitMap(Bundle savedInstanceState)
+        {
+            Bundle mapViewBundle = null;
+            if (savedInstanceState != null)
+            {
+                mapViewBundle = savedInstanceState.GetBundle("MapViewBundleKey");
+            }
+            mapView = FindViewById<MapView>(Resource.Id.googleMap);
+            mapView.OnCreate(mapViewBundle);
+            mapView.GetMapAsync(this);
+        }
+        public void InitToolbar()
+        {
+            Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+
+            CollapsingToolbarLayout collapsingToolbar = FindViewById<CollapsingToolbarLayout>(Resource.Id.collapsingToolbar);
+            AppBarLayout appBarLayout = FindViewById<AppBarLayout>(Resource.Id.appBar);
+            toolbar.Title = "";
+            SetSupportActionBar(toolbar);
+            var isShow = false;
+            var scrollRange = -1;
+
+            appBarLayout.OffsetChanged += (sender, args) =>
+            {
+                if (scrollRange == -1)
+                    scrollRange = appBarLayout.TotalScrollRange;
+                if (scrollRange + args.VerticalOffset == 0)
+                {
+                    collapsingToolbar.Title = "Mr. Piatto Restaurant";
+                    isShow = true;
+                }
+                else if (isShow)
+                {
+                    collapsingToolbar.Title = string.Empty;
+                    isShow = false;
+                }
+            };
+        }
+
+        
     }
 }
